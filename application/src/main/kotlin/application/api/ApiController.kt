52086889ac97c25b2bd01a566127b460cc8580a3
Christian Dräger <christian.draeger1@gmail.com>
2019-11-11 16:22:50 +0100

    add custom metric

--- application/src/main/kotlin/application/api/ApiController.kt
+++ application/src/main/kotlin/application/api/ApiController.kt
@@ -1,11 +1,19 @@
 package application.api
 
+import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ApiController {
+class ApiController(
+    meterRegistry: MeterRegistry
+) {
+
+    val counter = meterRegistry.counter("my.counter")
 
     @GetMapping("/hello")
-    fun getLocations() = "hello"
+    fun getLocations(): String {
+        counter.increment()
+        return "hello ${counter.count()}"
+    }
 }
