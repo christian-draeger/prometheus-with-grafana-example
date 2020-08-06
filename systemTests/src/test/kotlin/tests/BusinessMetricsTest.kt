05eed2429c39f4654d468da77def5c4c995a5b10
Christian Dräger <christian.draeger1@gmail.com>
2020-08-06 10:43:15 +0200

    adjust demo

--- systemTests/src/test/kotlin/tests/BusinessMetricsTest.kt
+++ systemTests/src/test/kotlin/tests/BusinessMetricsTest.kt
@@ -14,43 +14,40 @@ class BusinessMetricsTest : BrowserTest() {
 
     @Test
     fun `demo that metrics will be written to dashboard`() {
-        openBusinessMetricsDashboard
-        waitSeconds(5)
-        requestMiddlewareFromDifferentCountries
-        openBusinessMetricsDashboard
-        waitSeconds(10)
-        requestMiddlewareFromDifferentCountries
-        openBusinessMetricsDashboard
-        waitSeconds(10)
+        `Open Business Metrics Dashboard`
+        `wait seconds`(5)
+
+        `request Middleware From Different Countries`
+        `Open Business Metrics Dashboard`
+        `wait seconds`(10)
+
+        `request Middleware From Different Countries`
+        `Open Business Metrics Dashboard`
+        `wait seconds`(10)
     }
 
-    private val requestMiddlewareFromDifferentCountries: Unit
+    private val `request Middleware From Different Countries`: Unit
         get() {
-            doTimes(10) { callEndpoint(Locale.GERMANY) }
-            doTimes(5) { callEndpoint(Locale.ITALY) }
-            doTimes(3) { callEndpoint(Locale.FRANCE) }
-            doTimes(1) { callEndpoint(Locale.UK) }
+            repeat(10) { callEndpoint(Locale.GERMANY) }
+            repeat(5) { callEndpoint(Locale.ITALY) }
+            repeat(3) { callEndpoint(Locale.FRANCE) }
+            repeat(1) { callEndpoint(Locale.UK) }
         }
 
     private fun List<FluentWebElement>.findLinkByText(text: String) =
         find { it.text() == text } ?: throw Exception("could not find link with text: $text")
 
-    private fun doTimes(count: Int, callback: () -> Unit) {
-        for (x in 0 until count) callback()
-        Thread.sleep(1000)
-    }
-
     private fun callEndpoint(locale: Locale) {
         goTo("http://localhost:8080/example?country=${locale.country}")
     }
 
-    private val openBusinessMetricsDashboard: Unit
+    private val `Open Business Metrics Dashboard`: Unit
         get() {
             goTo("http://localhost:8080")
             jq("li a").findLinkByText("Business metrics").click()
         }
 
-    private fun waitSeconds(seconds: Long) {
+    private fun `wait seconds`(seconds: Long) {
         Thread.sleep(seconds * 1000)
     }
 }
